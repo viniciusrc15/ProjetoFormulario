f5e1fa2659adf7adb1871972844c9f234ffd494b
thaissantos <thaelisa@gmail.com>
2017-07-19 16:48:43 -0300

    alteraçoes css

--- app/public/js/geraPDF.js
+++ app/public/js/geraPDF.js
@@ -1,7 +1,7 @@
 $(document).ready( function() {
 	$(document).ready( function() {
 		$('#geraPdf').click(function() {
-			var doc = new jsPDF('p','pt', 'a3');
+			var doc = new jsPDF('Portrait', 'pt', 'a4');;
 			var elementHandler = {
             	'#fim': function (element, renderer) {
                 	return true;
@@ -10,7 +10,7 @@ $(document).ready( function() {
 			
 			console.log($('#conteudo').html());
 
-			doc.fromHTML($('#conteudo').html(), 15, 15, {
+			doc.fromHTML($('#conteudo').html(), 5, 5, {
 				'width': 500,
 				'elementHandlers': elementHandler
 			}, function(){doc.save("informacoes.pdf");});
