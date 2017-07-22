97b21da2ad2c38073b441efe2212859f24147d4d
viniciusrc15 <vinicius1595@gmail.com>
2017-07-22 16:46:46 -0300

    Download arquivo .doc

--- app/public/js/geraPDF.js
+++ app/public/js/geraPDF.js
@@ -1,25 +1,25 @@
 if (typeof jQuery !== "undefined" && typeof saveAs !== "undefined") {
-    (function($) {
-        $.fn.wordExport = function(fileName) {
-            fileName = typeof fileName !== 'undefined' ? fileName : "jQuery-Word-Export";
-            var static = {
-                mhtml: {
-                    top: "Mime-Version: 1.0\nContent-Base: " + location.href + "\nContent-Type: Multipart/related; boundary=\"NEXT.ITEM-BOUNDARY\";type=\"text/html\"\n\n--NEXT.ITEM-BOUNDARY\nContent-Type: text/html; charset=\"utf-8\"\nContent-Location: " + location.href + "\n\n<!DOCTYPE html>\n<html>\n_html_</html>",
-                    head: "<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n<style>\n_styles_\n</style>\n</head>\n",
-                    body: "<body>_body_</body>"
-                }
-            };
-            var options = {
-                maxWidth: 624
-            };
+	(function($) {
+		$.fn.wordExport = function(fileName) {
+			fileName = typeof fileName !== 'undefined' ? fileName : "jQuery-Word-Export";
+			var static = {
+				mhtml: {
+					top: "Mime-Version: 1.0\nContent-Base: " + location.href + "\nContent-Type: Multipart/related; boundary=\"NEXT.ITEM-BOUNDARY\";type=\"text/html\"\n\n--NEXT.ITEM-BOUNDARY\nContent-Type: text/html; charset=\"utf-8\"\nContent-Location: " + location.href + "\n\n<!DOCTYPE html>\n<html>\n_html_</html>",
+					head: "<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n<style>\n_styles_\n</style>\n</head>\n",
+					body: "<body>_body_</body>"
+				}
+			};
+			var options = {
+				maxWidth: 624
+			};
             // Clone selected element before manipulating it
             var markup = $(this).clone();
 
             // Remove hidden elements from the output
             markup.each(function() {
-                var self = $(this);
-                if (self.is(':hidden'))
-                    self.remove();
+            	var self = $(this);
+            	if (self.is(':hidden'))
+            		self.remove();
             });
 
             // Embed all images using Data URLs
@@ -43,21 +43,21 @@ if (typeof jQuery !== "undefined" && typeof saveAs !== "undefined") {
                 img[i].height = h;
                 // Save encoded image to array
                 images[i] = {
-                    type: uri.substring(uri.indexOf(":") + 1, uri.indexOf(";")),
-                    encoding: uri.substring(uri.indexOf(";") + 1, uri.indexOf(",")),
-                    location: $(img[i]).attr("src"),
-                    data: uri.substring(uri.indexOf(",") + 1)
+                	type: uri.substring(uri.indexOf(":") + 1, uri.indexOf(";")),
+                	encoding: uri.substring(uri.indexOf(";") + 1, uri.indexOf(",")),
+                	location: $(img[i]).attr("src"),
+                	data: uri.substring(uri.indexOf(",") + 1)
                 };
             }
 
             // Prepare bottom of mhtml file with image data
             var mhtmlBottom = "\n";
             for (var i = 0; i < images.length; i++) {
-                mhtmlBottom += "--NEXT.ITEM-BOUNDARY\n";
-                mhtmlBottom += "Content-Location: " + images[i].location + "\n";
-                mhtmlBottom += "Content-Type: " + images[i].type + "\n";
-                mhtmlBottom += "Content-Transfer-Encoding: " + images[i].encoding + "\n\n";
-                mhtmlBottom += images[i].data + "\n\n";
+            	mhtmlBottom += "--NEXT.ITEM-BOUNDARY\n";
+            	mhtmlBottom += "Content-Location: " + images[i].location + "\n";
+            	mhtmlBottom += "Content-Type: " + images[i].type + "\n";
+            	mhtmlBottom += "Content-Transfer-Encoding: " + images[i].encoding + "\n\n";
+            	mhtmlBottom += images[i].data + "\n\n";
             }
             mhtmlBottom += "--NEXT.ITEM-BOUNDARY--";
 
@@ -69,26 +69,32 @@ if (typeof jQuery !== "undefined" && typeof saveAs !== "undefined") {
 
             // Create a Blob with the file contents
             var blob = new Blob([fileContent], {
-                type: "application/msword;charset=utf-8"
+            	type: "application/msword;charset=utf-8"
             });
             saveAs(blob, fileName + ".doc");
         };
     })(jQuery);
 } else {
-    if (typeof jQuery === "undefined") {
-        console.error("jQuery Word Export: missing dependency (jQuery)");
-    }
-    if (typeof saveAs === "undefined") {
-        console.error("jQuery Word Export: missing dependency (FileSaver.js)");
-    }
+	if (typeof jQuery === "undefined") {
+		console.error("jQuery Word Export: missing dependency (jQuery)");
+	}
+	if (typeof saveAs === "undefined") {
+		console.error("jQuery Word Export: missing dependency (FileSaver.js)");
+	}
 }
 
 
 $(document).ready( function() {
 
-$("a.jquery-word-export").click(function(event) {
-            $("#page-content").wordExport();
-        });
+	$("a.jquery-word-export").click(function(event) {
+		$("#page-content").wordExport();
+	});
+	$('#geraPdf').click(function() {
+		var doc = new jsPDF('Portrait', 'pt', 'a4');
+		doc.addHTML($('body'), function() {
+			doc.save("informacoes.pdf");
+		});
+	});
 
 });
 
