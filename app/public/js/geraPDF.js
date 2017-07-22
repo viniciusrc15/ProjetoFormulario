e9dad747117c51800bb96c05bf80e6d512e54f70
viniciusrc15 <vinicius1595@gmail.com>
2017-07-22 13:50:16 -0300

    Modificando jeito de gerar PDF

--- app/public/js/geraPDF.js
+++ app/public/js/geraPDF.js
@@ -1,18 +1,14 @@
-/*//Responsavel por gerar PDF
+//Responsavel por gerar PDF
 $(document).ready( function() {
 	//Imprime certo porem hora sim hora nao
 	console.log($(window).load());
 	$('#geraPdf').click(function() {
-<<<<<<< HEAD
 		var doc = new jsPDF('Landscap', 'pt', 'a4');
-=======
-		var doc = new jsPDF('Landscape', 'pt', 'a4');
->>>>>>> 2396c2d5bc27b28b4adc99a8c07208d4ce04a942
-		doc.addHTML($('#conteudo'), function() {
+		doc.addHTML($('body'), function() {
 			doc.save("informacoes.pdf");
 		});
 	});
-
+});
 	/*
 		imprime desconfigurado
 
@@ -55,37 +51,3 @@ $(document).ready( function() {
 	});
 });
 	*/
-
-$(document).ready( function() {
-	$(document).ready( function() {
-		/*
-		 * Aqui é função que o botão chama */
-
-		$('#geraPDF').click(function() {
-
-			alert("Vamos conseguir");
-			
-			PDFJS.workerSrc = "js/pdfjs/build/pdf.worker.js";
-			PDFJS.disableWorker = true; 
-			PDFJS.getDocument('informacoes.pdf');
-			PDFJS.getDocument('informacoes.pdf').then(function(pdf) {
-
-				var scale = 1.5;
-				var viewport = page.getViewport(scale);
-
-				var canvas = document.getElementById('the-canvas');
-				var context = canvas.getContext('2d');
-				canvas.height = viewport.height;
-				canvas.width = viewport.width;
-
-				var renderContext = {
-				  canvasContext: context,
-				  viewport: viewport
-				};
-				page.render(renderContext);
-			 
-			});
-			
-		});
-	});
-});
